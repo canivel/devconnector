bd34efad1c6fd889aa5b63daca5ecf1fec5c0b0a
dcanivel-at-325274068097 <IESzg7L/tD0VMsuy/YKjuPgawtjynvmwD+rO24MtpGs=>
2018-12-29 23:18:05 -0500

    start adding routes

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
+const keys = require("./config/keys");
+
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
 
 const app = express();
+
+mongoose
+  .connect(keys.mongoURI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
 
 app.get("/", (req, res) => {
   res.send("Hello");
 });
+
+//Use Routes
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
 
 const port = process.env.PORT || 5000;
 
