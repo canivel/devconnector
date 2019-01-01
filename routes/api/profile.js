436db440b974911b2d15ba95dc3f73d01da4e072
dcanivel-at-325274068097 <IESzg7L/tD0VMsuy/YKjuPgawtjynvmwD+rO24MtpGs=>
2019-01-01 18:06:28 -0500

    start creating the profile crud routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
+
+//Load models
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
 
 // @route   GET api/profile/test
 // @desc    Test Profile Route
@@ -8,4 +14,75 @@ router.get("/test", (req, res) => {
   res.json({ msg: "Profile Works" });
 });
 
+// @route   GET api/profile
+// @desc    Get Current User Profile Route
+// @access  Private
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    const errors = {};
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+      if (!profile) {
+        errors.noprofile = "There is no profile for this user";
+        return res.status(404).json(errors);
+      }
+      res.json(profile);
+    } catch (err) {
+      return res.status(404).json(err);
+    }
+  }
+);
+
+// @route   POST api/profile
+// @desc    Create User Profile Route
+// @access  Private
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    // Get fields
+    const {
+      skills,
+      youtube,
+      twitter,
+      facebook,
+      linkedin,
+      instagram
+    } = req.body;
+    const profileFields = {
+      ...req.body,
+      user: req.user.id,
+      skills: skills.split(","),
+      social: { youtube, twitter, facebook, linkedin, instagram }
+    };
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+      if (profile) {
+        //update profile
+        const updatedProfile = await Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true }
+        );
+        return res.json(updatedProfile);
+      } else {
+        // create
+        const checkProfile = await Profile.findOne({
+          handle: profileFields.handle
+        });
+        if (checkProfile) {
+          errors.handle = "That handle already exists";
+          res.status(400).json(errors);
+        }
+
+        const newProfile = await new Profile(profileFields).save();
+        return res.json(newProfile);
+      }
+    } catch (err) {
+      return res.status(404).json(err);
+    }
+  }
+);
 module.exports = router;
