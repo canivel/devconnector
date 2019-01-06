13cc3a5f9cf0cb9e7d314f38b23ddcfde7eb6636
dcanivel-at-325274068097 <IESzg7L/tD0VMsuy/YKjuPgawtjynvmwD+rO24MtpGs=>
2019-01-06 15:05:41 -0500

    add history

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,25 +1,28 @@
 import React, { Component } from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { Router, Route, Switch } from "react-router-dom";
 import Navbar from "./layout/Navbar";
 import Footer from "./layout/Footer";
 import Landing from "./layout/Landing";
 import Register from "./auth/Register";
 import Login from "./auth/Login";
+import history from "../history";
 
 export class App extends Component {
   render() {
     return (
-      <BrowserRouter>
+      <Router history={history}>
         <div>
           <Navbar />
-          <Route path="/" exact component={Landing} />
-          <div className="container">
-            <Route path="/register" exact component={Register} />
-            <Route path="/login" exact component={Login} />
-          </div>
+          <Switch>
+            <Route path="/" exact component={Landing} />
+            <div className="container">
+              <Route path="/register" exact component={Register} />
+              <Route path="/login" exact component={Login} />
+            </div>
+          </Switch>
           <Footer />
         </div>
-      </BrowserRouter>
+      </Router>
     );
   }
 }
